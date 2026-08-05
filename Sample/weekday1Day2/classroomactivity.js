9c8f2a0d75e142c9cb8106c0afe1501551a3426a
vsathya09-spec <vsathya09@gmail.com>
2026-08-05 09:50:57 +0530

    Add files via upload
    Week1Day2

---
+++ Sample/weekday1Day2/classroomactivity.js
@@ -0,0 +1,35 @@
+//firstName
+var firstName = "Sathya";
+console.log(firstName);
+console.log(typeof firstName);
+
+//companyName
+var companyName = "CTS";
+console.log(companyName);
+console.log(typeof companyName);
+
+//mobileNumber
+var mobileNumber = 1234567890;
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+//isAutomation
+var isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+//undefined
+var hasplaywright;
+console.log(hasplaywright);
+console.log(typeof hasplaywright);
+
+//null
+var hasplaywright = null;
+console.log(hasplaywright);
+console.log(typeof hasplaywright);
+
+//bigInt
+var bigInt = 1234567890123456789012345678901234567890n;
+console.log(bigInt);
+console.log(typeof bigInt);
+
